b2c8709ddde8a12e7b2a607bb861ceeaa04326aa
mxmar <macht4tech@gmail.com>
2021-10-11 14:33:08 +0200

    Provided SECOND_PER_SLOTS parameter
    Signed-off-by: mxmar <macht4tech@gmail.com>

--- src/app/store/Stats.ts
+++ src/app/store/Stats.ts
@@ -4,6 +4,7 @@ import {Store} from "app/store/Store";
 export class Stats {
     @observable currentSlot: number;
     @observable currentEpoch: number;
+    @observable secondsPerSlot: number;
     @observable timeSinceSlotStart = this.formatTime(0);
 
     main: Store;
@@ -15,6 +16,7 @@ export class Stats {
         this.networkGenesisTime = Math.floor(
             new Date(networkGenesisTime).getTime() / 100) / 10;
         this.interval = setInterval(() => { this.calculateCurrentState(); }, 100);
+        this.secondsPerSlot = this.main.getNetworkConfig()!.SECONDS_PER_SLOT;
     }
 
     stop() {
@@ -23,20 +25,21 @@ export class Stats {
         this.currentSlot = 0;
         this.currentEpoch = 0;
         this.timeSinceSlotStart = this.formatTime(0);
+        this.secondsPerSlot = this.main.getNetworkConfig()!.SECONDS_PER_SLOT;
     }
 
     calculateCurrentState() {
         let now = Math.floor((new Date()).getTime() / 100) / 10;
 
-        this.currentSlot = Math.floor((now - this.networkGenesisTime) / this.main.getNetworkConfig()!.SECONDS_PER_SLOT);
+        this.currentSlot = Math.floor((now - this.networkGenesisTime) / this.secondsPerSlot);
         this.currentEpoch = Math.floor(this.currentSlot / 32);
         this.timeSinceSlotStart = this.formatTime(Math.round(
-            ((now - this.networkGenesisTime) - this.currentSlot * this.main.getNetworkConfig()!.SECONDS_PER_SLOT) * 10) / 10);
+            ((now - this.networkGenesisTime) - this.currentSlot * this.secondsPerSlot) * 10) / 10);
     }
 
     formatTime(t: any) {
         if (t < 0) { t = 0; }
-        if (t > this.main.getNetworkConfig()!.SECONDS_PER_SLOT) { t = this.main.getNetworkConfig()!.SECONDS_PER_SLOT; }
+        if (t > this.secondsPerSlot) { t = this.secondsPerSlot; }
 
         t += "";
 
